7890ce8c63c1cef386c7703adfa99ba4ea7d6813
AliReza <torkamani.alireza@gmail.com>
2018-07-02 23:15:44 -0700

    landing page added

---
+++ landingpage/app.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response){
+
+    console.log('client request URL: ', request.url);
+
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/ninjas") {
+         fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-Type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if (request.url === "/ninjas/new") {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else {
+        response.end('Requested URL is not available!');
+    }
+});
+
+server.listen(6789);
+console.log("Running in localhost at port 6789");
